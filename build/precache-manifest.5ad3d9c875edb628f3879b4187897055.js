85d1380e6d2dc846ed104acfdae39370bdeebd73
zhengranqiu <zhengranqiu@maoyan.com>
2020-01-13 15:59:34 +0800

    add: build

---
+++ build/precache-manifest.5ad3d9c875edb628f3879b4187897055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd69c12a26b7af03c96565f3c6c360cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ce8fc13db15d17c00c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81aef7176c9f9805ca1e",
+    "url": "/static/js/2.99ff2568.chunk.js"
+  },
+  {
+    "revision": "ba713588e2b4862a01abb9dea6df4b54",
+    "url": "/static/js/2.99ff2568.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4ce8fc13db15d17c00c",
+    "url": "/static/js/main.38e5f41a.chunk.js"
+  },
+  {
+    "revision": "c909086cd79fd532d748",
+    "url": "/static/js/runtime-main.d6d3e7aa.js"
+  }
+]);
